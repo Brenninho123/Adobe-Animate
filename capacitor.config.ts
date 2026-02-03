bae205a2d0f2b20f58ab5b1042799cbfd8851835
Ander <157061485+Brenninho123@users.noreply.github.com>
2026-02-03 16:27:45 -0300

    Create capacitor.config.ts

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.animate.html',
+  appName: 'Adobe Animate',
+  webDir: '.',
+  bundledWebRuntime: false
+};
+
+export default config;
